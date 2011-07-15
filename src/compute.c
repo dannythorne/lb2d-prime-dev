b8453c78bcaf718b124b3c95a0445b84afeb2fa2
andrew.j.pearson@gmail.com <andrew.j.pearson@gmail.com>
2011-07-15 16:47:13 -0400

    Corrected bug in definitions of U_X_BIG and U_Y_BIG.  The gravitational force contribution was being divided by density incorrectly.

--- src/compute.c
+++ src/compute.c
@@ -40,22 +40,18 @@
         + lattice->param.tau[subs]  \
           * lattice->force[subs][n].force[0]/(rho_) \
         + lattice->param.tau[subs]  \
-          /** lattice->force[subs][n].sforce[0]/(rho_) */\
-          /** lattice->force[subs][n].sforce[0]*(rho_) */\
           * lattice->force[subs][n].sforce[0] \
         + lattice->param.tau[subs]  \
-          * lattice->param.gval[subs][0]/(rho_)
+          * lattice->param.gval[subs][0]
 
 #define BIG_U_Y( u_, rho_) \
         (u_) \
         + lattice->param.tau[subs]  \
           * lattice->force[subs][n].force[1]/(rho_) \
         + lattice->param.tau[subs]  \
-          /** lattice->force[subs][n].sforce[1]/(rho_) */\
-          /** lattice->force[subs][n].sforce[1]*(rho_) */\
           * lattice->force[subs][n].sforce[1] \
         + lattice->param.tau[subs]  \
-          * lattice->param.gval[subs][1]/(rho_)
+          * lattice->param.gval[subs][1]
 
 #endif /* ZHANG_AND_CHEN_ENERGY_TRANSPORT */
 
